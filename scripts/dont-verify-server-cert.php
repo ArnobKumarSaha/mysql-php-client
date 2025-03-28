aa608b60d4a0c4d4fe3212c52cdf1116e97ee18b
Arnob Kumar Saha <arnob@appscode.com>
2025-03-28 17:15:44 +0600

    Regenaret the issue by inserting random data
    Signed-off-by: Arnob Kumar Saha <arnob@appscode.com>

--- scripts/dont-verify-server-cert.php
+++ scripts/dont-verify-server-cert.php
@@ -1,6 +1,4 @@
 <?php
-// https://github.com/php/php-src/issues/8978 
-
 $host = "10.2.0.183:10002";
 $user = "root";
 $database = "mysql";
@@ -10,6 +8,7 @@ $ca_cert = "/Users/arnobkumarsaha/go/src/github.com/ArnobKumarSaha/mysql-php-cli
 // $port = 10007;
 // $password = "NK)9(YIN4lj.YSzg"; 
 $password = "fWN1ibPIy5RvgNAp"; 
+$row = 100;
 
 
 $connect = mysqli_init();
@@ -17,23 +16,54 @@ if (!$connect) {
     throw new Exception('mysqli_init failed');
 }
 
-
-
 $flags = MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT;
-// Didnt work with this paramet
-// $connect->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
-
-
-
 mysqli_ssl_set($connect, null, null, $ca_cert, null, null);
-// mysqli_real_connect($connect, $host, $user, $password, null, null, null, $flags);
-
 
 if (!mysqli_real_connect($connect, $host, $user, $password, $database, null, null, $flags)) {
     throw new Exception('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
 }
-// }
-echo "ALL Good";
-$connect->close();
 
+// Create table if not exists
+$table_query = "CREATE TABLE IF NOT EXISTS random_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    random_text VARCHAR(255),
+    random_number INT
+)";
+if (!$connect->query($table_query)) {
+    throw new Exception('Table creation failed: ' . $connect->error);
+}
+
+// Insert 500 random rows
+$insert_query = $connect->prepare("INSERT INTO random_data (random_text, random_number) VALUES (?, ?)");
+if (!$insert_query) {
+    throw new Exception('Insert statement preparation failed: ' . $connect->error);
+}
+
+for ($i = 0; $i < $row; $i++) {
+    $random_text = "Text_" . bin2hex(random_bytes(4));
+    $random_number = rand(1, 1000);
+    $insert_query->bind_param("si", $random_text, $random_number);
+    $insert_query->execute();
+}
+$insert_query->close();
+
+// Retrieve the inserted data
+$result = $connect->query("SELECT * FROM random_data LIMIT 10"); // Fetch first 10 rows for display
+
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row['id'] . " | Text: " . $row['random_text'] . " | Number: " . $row['random_number'] . "<br>";
+    }
+    $result->free();
+} else {
+    throw new Exception('Data retrieval failed: ' . $connect->error);
+}
+
+echo "ALL Good";
+
+$drop_query = "DROP TABLE IF EXISTS random_data";
+if (!$connect->query($drop_query)) {
+    throw new Exception('Table drop failed: ' . $connect->error);
+}
+$connect->close();
 ?>
